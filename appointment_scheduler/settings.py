d403e9fe0231b20faa8e6f2d030cd099d485a287
Melvin Thomas <melvinchooranolil@gmail.com>
2021-08-14 14:36:46 +0530

    bd config update

--- appointment_scheduler/settings.py
+++ appointment_scheduler/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'appointment_scheduler.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 db_from_env=dj_database_url.config(conn_max_age=500)
